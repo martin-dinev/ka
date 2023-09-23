0c3f9b85eaf4005353c97ab8d9c9abd793b1b5c6
Martin <martin.dinev@students.finki.ukim.mk>
2023-09-23 11:33:49 +0200

    allow cube point modeling

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {GUI} from "three/addons/libs/lil-gui.module.min";
 import {TransformControls} from "three/addons/controls/TransformControls";
 import {Toolbar} from "./Toolbar";
+import {mergeVertices} from "three/addons/utils/BufferGeometryUtils";
+import { Points} from "three";
 
 if (!WebGL.isWebGLAvailable()) {
     const warning = WebGL.getWebGLErrorMessage();
@@ -13,7 +15,8 @@ if (!WebGL.isWebGLAvailable()) {
     let container, stats, gui;
     let camera, scene, renderer;
     let controls, transformControls;
-    let editorSignals, selected, selectedShowPoints, onTransformShowEditPoints, isTheSelectedPoint;
+    let editorSignals, selection;
+    let showEditPoints = false;
 
 
     // const pointer = new THREE.Vector2();
@@ -54,7 +57,7 @@ if (!WebGL.isWebGLAvailable()) {
 
         scene = new THREE.Scene();
         scene.add(objectsGroup);
-        scene.background = new THREE.Color(0xf0f0f0);
+        scene.background = new THREE.Color(0x555555);
         scene.add(new THREE.AxesHelper(2000));
 
         camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
@@ -160,43 +163,178 @@ if (!WebGL.isWebGLAvailable()) {
     }
 
     function addCube() {
-        let cubeGeometry = new THREE.BoxGeometry(250, 250, 250);
-        let cubeMesh = new THREE.MeshPhongMaterial({color: Math.random() * 0xffffff});
-        let cube = new THREE.Mesh(cubeGeometry, cubeMesh);
         let group = new THREE.Group();
+        // let verticiesPositions = new Float32Array([
+        //     0, 0, 0,
+        //     0, 0, 250,
+        //     0, 250, 0,
+        //     0, 250, 250,
+        //     250, 0, 0,
+        //     250, 0, 250,
+        //     250, 250, 0,
+        //     250, 250, 250
+        // ]);
+        // let index = [
+        //     0, 1, 2, 3, 2, 1,
+        //     6, 5, 4, 5, 6, 7,
+        //     4, 1, 0, 1, 4, 5,
+        //     2, 3, 6, 7, 6, 3,
+        //     0, 2, 4, 6, 4, 2,
+        //     5, 3, 1, 3, 5, 7
+        // ];
 
+        let verticies = new THREE.Group();
         //
-        // let mergedVerticies = new THREE.BoxGeometry( 250, 250, 250);
-        // mergedVerticies.deleteAttribute( 'normal' );
-        // mergedVerticies.deleteAttribute( 'uv' );
-        // mergedVerticies = mergeVertices( mergedVerticies );
-        // let positionAttribute = mergedVerticies.getAttribute("position");
+        // let cubeGeometry = new THREE.BufferGeometry();
+        // cubeGeometry.setIndex(index);
+        // cubeGeometry.setAttribute('position', new THREE.BufferAttribute(verticiesPositions, 3));
         //
-        // const colors = [];
-        // const sizes = [];
+        // cubeGeometry.translate(250, 250, 250);
+        // group.add(new THREE.Mesh(cubeGeometry, new THREE.MeshBasicMaterial({
+        //     color: Math.random() * 0xffffff,
+        //     // wireframe: true,
+        // })));
+
+
+        let cubeGeometry = new THREE.BoxGeometry(250, 250, 250);
+        cubeGeometry.deleteAttribute('normal');
+        cubeGeometry.deleteAttribute('uv');
+        cubeGeometry = mergeVertices(cubeGeometry);
+
+
+        // const triangles = cubeGeometry.index.length / 3;
+        // const positions = cubeGeometry.getAttribute( 'position' ).array;
+        // const normals = new Float32Array( triangles * 3 * 3 );
+        // const colors = new Float32Array( triangles * 3 * 3 );
         //
         // const color = new THREE.Color();
         //
-        // for ( let i = 0, l = positionAttribute.count; i < l; i ++ ) {
+        // const pA = new THREE.Vector3();
+        // const pB = new THREE.Vector3();
+        // const pC = new THREE.Vector3();
         //
-        //     color.setHSL( 0.01 + 0.1 * ( i / l ), 1.0, 0.5 );
-        //     color.toArray( colors, i * 3 );
+        // const cb = new THREE.Vector3();
+        // const ab = new THREE.Vector3();
         //
-        //     sizes[ i ] = 100;
+        // for ( let i = 0; i < positions.length; i += 9 ) {
+        //
+        //
+        //     pA.set( positions[ i ], positions[ i + 1 ], positions[ i + 2 ] );
+        //     pB.set( positions[ i + 3 ], positions[ i + 4 ], positions[ i + 5 ] );
+        //     pC.set( positions[ i + 6 ], positions[ i + 7 ], positions[ i + 8 ] );
+        //
+        //     cb.subVectors( pC, pB );
+        //     ab.subVectors( pA, pB );
+        //     cb.cross( ab );
+        //
+        //     cb.normalize();
+        //
+        //     const nx = cb.x;
+        //     const ny = cb.y;
+        //     const nz = cb.z;
+        //
+        //     normals[ i ] = nx;
+        //     normals[ i + 1 ] = ny;
+        //     normals[ i + 2 ] = nz;
+        //
+        //     normals[ i + 3 ] = nx;
+        //     normals[ i + 4 ] = ny;
+        //     normals[ i + 5 ] = nz;
+        //
+        //     normals[ i + 6 ] = nx;
+        //     normals[ i + 7 ] = ny;
+        //     normals[ i + 8 ] = nz;
+        //
+        //     // colors
+        //
+        //     // const vx = ( x / n ) + 0.5;
+        //     // const vy = ( y / n ) + 0.5;
+        //     // const vz = ( z / n ) + 0.5;
+        //     //
+        //     // color.setRGB( vx, vy, vz );
+        //     //
+        //     // colors[ i ] = color.r;
+        //     // colors[ i + 1 ] = color.g;
+        //     // colors[ i + 2 ] = color.b;
+        //     //
+        //     // colors[ i + 3 ] = color.r;
+        //     // colors[ i + 4 ] = color.g;
+        //     // colors[ i + 5 ] = color.b;
+        //     //
+        //     // colors[ i + 6 ] = color.r;
+        //     // colors[ i + 7 ] = color.g;
+        //     // colors[ i + 8 ] = color.b;
         //
         // }
         //
+        // // geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+        // cubeGeometry.setAttribute( 'normal', new THREE.BufferAttribute( normals, 3 ) );
+        // // geometry.setAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
         //
-        // let verticiesGeometry = new THREE.BufferGeometry();
-        // verticiesGeometry.setAttribute( "position", positionAttribute);
+
+        let normlas = new Float32Array(cubeGeometry.attributes.position.array.length);
+
+        for (let i = 0 ; i < cubeGeometry.attributes.position.array.length; i += 3) {
+            let pA = new THREE.Vector3(cubeGeometry.attributes.position.array[i], cubeGeometry.attributes.position.array[i + 1], cubeGeometry.attributes.position.array[i + 2]);
+             pA.normalize();
+            normlas[i] = pA.x;
+            normlas[i + 1] = pA.y;
+            normlas[i + 2] = pA.z;
+        }
+
+        cubeGeometry.setAttribute('normal', new THREE.BufferAttribute(normlas, 3));
+
+        cubeGeometry.computeBoundingSphere();
 
 
 
-        let verticies = new THREE.Points(cubeGeometry, new THREE.PointsMaterial({color: 0xff00ff, size: 15, sizeAttenuation: false}));
-        verticies.visible = false;
+
+
+
+
+
+
+
+
+
+
+
+
+        let coordinates = cubeGeometry.getAttribute('position').array;
+        // console.log(cubeGeometry.getAttribute('position'));
+        for (let i = 0; i < coordinates.length; i += 3) {
+            let point = new THREE.Vector3(coordinates[i], coordinates[i + 1], coordinates[i + 2]);
+            let helper_object = new Points((new THREE.BufferGeometry()).setFromPoints([new THREE.Vector3(0, 0, 0)]),
+                new THREE.PointsMaterial({
+                    color: 0xff00ff,
+                    size: 15,
+                    sizeAttenuation: false
+                })
+
+                );
+            helper_object.userData.index = i / 3;
+            helper_object.position.copy(point);
+            helper_object.visible = false;
+            verticies.add(helper_object);
+        }
+
+        // verticies = new THREE.Points(cubeGeometry, new THREE.PointsMaterial({
+        //     color: 0xff00ff, size: 15, sizeAttenuation: false
+        // }));
+
+        // console.log(verticies);
+
+        let cubeMesh = new THREE.MeshNormalMaterial({color: Math.random() * 0xffffff, wireframe: false,});
+        // cubeGeometry = ;
+        let cube = new THREE.Mesh(cubeGeometry, cubeMesh);
+
+        // verticies.visible = false;
 
         group.add(verticies);
         group.add(cube);
+        group.translateX(125);
+        group.translateY(125);
+        group.translateZ(125);
         objectsGroup.add(group);
         render();
     }
@@ -252,8 +390,7 @@ if (!WebGL.isWebGLAvailable()) {
             transformModeChanged: new Signal(),
             snapChanged: new Signal(),
             spaceChanged: new Signal(),
-            editPointsChanged: new Signal(),
-            // rendererCreated: new Signal(),
+            editPointsChanged: new Signal(), // rendererCreated: new Signal(),
             // rendererUpdated: new Signal(),
 
             // sceneBackgroundChanged: new Signal(),
@@ -268,8 +405,7 @@ if (!WebGL.isWebGLAvailable()) {
 
             // geometryChanged: new Signal(),
 
-            objectSelected: new Signal(),
-            // objectFocused: new Signal(),
+            selectionChanged: new Signal(), // objectFocused: new Signal(),
             //
             // objectAdded: new Signal(),
             // objectChanged: new Signal(),
@@ -321,11 +457,9 @@ if (!WebGL.isWebGLAvailable()) {
 
         });
 
-        editorSignals.editPointsChanged.add(function (showEditPoints) {
-            onTransformShowEditPoints = showEditPoints;
-            if(selected != null){
-                editorSignals.intersectionsDetected.dispatch(selected);
-            }
+        editorSignals.editPointsChanged.add(function (newShowEditPoints) {
+            showEditPoints = newShowEditPoints;
+            editorSignals.selectionChanged.dispatch(selection);
             render();
         });
     }
@@ -353,6 +487,29 @@ if (!WebGL.isWebGLAvailable()) {
                 // }
 
                 // signals.refreshSidebarObject3D.dispatch( object );
+
+                if(showEditPoints){
+                    if(isSelectedPoint(selection.object)){
+                        let group_top = getParent(object);
+                        let group_geometry = group_top.children[1].geometry;
+                        let geometry_position = group_geometry.getAttribute("position");
+                        let new_position = selection.object.position;
+                        // console.log();
+                        // console.log(geometry_position);
+                        geometry_position.setXYZ(selection.object.userData.index, new_position.x, new_position.y, new_position.z);
+                        // console.log(geometry_position)
+                        //
+                        // geometry_position.copyAt(selection.index,selection.object.position);
+                        // console.log(geometry_position);
+
+
+
+                        group_geometry.getAttribute("position").needsUpdate = true;
+                        // group_geometry.attributes.position.needsUpdate = true;
+                        // console.log(group_geometry);
+                        // console.log(group_points);
+                    }
+                }
 
             }
 
@@ -426,45 +583,37 @@ if (!WebGL.isWebGLAvailable()) {
 
             if (intersects.length > 0) {
 
-                const object = intersects[0].object;
+                const new_selection = intersects[0];
 
-                if (object.userData.object !== undefined) {
-
+                if (new_selection.object.userData.object !== undefined) {
+                    // todo maybe
                     // helper
-
-                    select(object.userData.object);
-
+                    select(new_selection.object.userData.object);
                 } else {
-
-                    select(object);
-
+                    select(new_selection);
                 }
-
             } else {
-
                 select(null);
-
             }
 
         });
 
 
-        editorSignals.objectSelected.add(function (object) {
+        editorSignals.selectionChanged.add(function (new_selection) {
 
             // selectionBox.visible = false;
-            if(selected != null){
-                if(selectedShowPoints){
-                    selected.traverse(function (child) {
+            if (selection != null) {
+                if (showEditPoints) {
+                    getParent(selection.object).traverse(function (child) {
                         if (child instanceof THREE.Points) {
                             child.visible = false;
                         }
                     });
                 }
-                selected = null;
+                selection = null;
             }
             transformControls.detach();
-
-            if (object !== null && object !== scene && object !== camera) {
+            if (!(new_selection === null || new_selection === undefined) && new_selection.object !== scene && new_selection.object !== camera) {
 
                 // box.setFromObject( object, true );
 
@@ -474,27 +623,38 @@ if (!WebGL.isWebGLAvailable()) {
                 //
                 // }
 
-                selected = getParent(object);
+                selection = new_selection;
 
 
-                if(onTransformShowEditPoints){
-                    selectedShowPoints = true;
-                    selected.traverse(function (child) {
+                if (showEditPoints) {
+                    getParent(selection.object).traverse(function (child) {
                         if (child instanceof THREE.Points) {
                             child.visible = true;
                         }
                     });
-                }else{
-                    selectedShowPoints = false;
                 }
+                if (isSelectedPoint(selection.object)) {
+                    // console.log(selection.index)
+                    // let points = selection.object.geometry.attributes.position.array;
+                    // for (let i = 0; i < points.length; i += 3) {
+                    //     console.log(points[i], points[i + 1], points[i + 2])
+                    // }
+                    // console.log(points)
 
-                isTheSelectedPoint = isSelectedPoint(object);
-                if (isTheSelectedPoint) {
-                    transformControls.attach(object);
+                    // let v = selection.object.geometry.attributes.position.array.slice(selection.index * 3,selection.index * 3 + 3);
+                    // let point = new THREE.Vector3(v[0], v[1], v[2]);
+                    //
+                    // let helper_object = new Points((new THREE.BufferGeometry()).setFromPoints([new THREE.Vector3(0, 0, 0)]));
+                    // helper_object.position.copy(point);
+                    // helper_object.visible=false;
+                    // scene.add(helper_object);
+                    // transformControls.attach(helper_object);
+
+                    transformControls.attach(selection.object);
+
+                } else {
+                    transformControls.attach(getParent(selection.object));
                 }
-else
-                transformControls.attach(getParent(object));
-
             }
 
             render();
@@ -504,9 +664,9 @@ else
 
     }
 
-    function getParent(object){
-        while(object.parent!==objectsGroup){
-            object=object.parent
+    function getParent(object) {
+        while (object.parent !== objectsGroup) {
+            object = object.parent
         }
         return object
     }
@@ -557,24 +717,9 @@ else
 
     }
 
-    function select(object) { //todo
-        let objectParent = null
-        if (object != null) objectParent = getParent(object);
-
-        if (selected === objectParent && selectedShowPoints === onTransformShowEditPoints && isTheSelectedPoint === isSelectedPoint(object) ) return;
-
-        let uuid = null;
-
-        if (objectParent !== null) {
-
-            uuid = objectParent.uuid;
-
-        }
-
-        // config.setKey( 'selected', uuid );
-
-        editorSignals.objectSelected.dispatch(object);
-
+    function select(new_selection) { //todo
+        if (new_selection === selection) return;
+        editorSignals.selectionChanged.dispatch(new_selection);
     }
 
     function deselect() {
